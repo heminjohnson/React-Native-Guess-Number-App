db3fd428c1bd769d2ba4fc30542ff15bd493078b
Hemin Johnson <achayan1992@gmail.com>
2019-08-21 16:18:19 +0200

    feat: update the output text on the game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, StyleSheet, Button, Image } from 'react-native'
+import { View, StyleSheet, Button, Image, Text } from 'react-native'
 
 import BodyText from '../components/BodyText'
 import TitleText from '../components/Titletext'
+import Colors from '../constants/colors'
 
 const GameOverScreen = props => {
   return (
@@ -10,13 +11,18 @@ const GameOverScreen = props => {
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
         <Image
-          // source={require('../assets/success.png')}
-          source={{ uri: 'https://www.telegraph.co.uk/content/dam/Travel/ski/K2-mountain-Andrzej-Bargiel-first-ski-descent-by-Piotr-Pawlus-Red-Bull-Content-Pool.jpg?imwidth=1400' }}
+          source={require('../assets/success.png')}
+          // source={{ uri: 'https://www.telegraph.co.uk/content/dam/Travel/ski/K2-mountain-Andrzej-Bargiel-first-ski-descent-by-Piotr-Pawlus-Red-Bull-Content-Pool.jpg?imwidth=1400' }}
           style={styles.image}
           resizeMode={'cover'}/>
       </View>
-      <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed <Text style={styles.highlight}>{props.roundsNumber} </Text>
+          rounds to guess the number
+          <Text style={styles.highlight}> {props.userNumber} </Text>
+        </BodyText>
+      </View>
       <Button title={'NEW GAME'} onPress={props.onRestart}/>
     </View>
   )
@@ -40,6 +46,18 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%'
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold'
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20
   }
 })
 
